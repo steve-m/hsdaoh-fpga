1b2d0115816669986399ac8a9637dadb1873c74d
Steve Markgraf <steve@steve-m.de>
2024-12-06 00:21:09 +0100

    hsdaoh_core: add CRC16 checksum support
    Output is identical to the implementation in
    hsdaoh-rp2350.

--- common/hsdaoh/hsdaoh_core.v
+++ common/hsdaoh/hsdaoh_core.v
@@ -18,6 +18,21 @@ module hsdaoh_core
 	input wire [15:0] data_in
 );
 
+	parameter USE_CRC = 1;
+
+	reg crc_enable = 1'b0;
+	wire [15:0] crc_out;
+	reg [15:0] last_line_crc;
+
+	crc16_ccitt crc16_ccitt (
+		.CLK(clk_pixel),
+		.RSTn(rstn),
+		.data_in({hdmi_data[15:8], hdmi_data[23:16]}),
+		.enable(crc_enable),
+		.clear(!crc_enable),
+		.CRC(crc_out)
+	);
+
 	localparam [31:0] MAGIC = 32'hda7acab1;
 
 	reg [23:0] hdmi_data = 24'h000000;
@@ -35,38 +50,54 @@ module hsdaoh_core
 
 always @(posedge clk_pixel) begin
 
-	if ((cx == screen_width-1) && (cy < screen_height)) begin
-		// last word of line contains counter of words per line
-		hdmi_data <= {status_nibble[3:0], line_word_cnt[11:0], 8'h00};
+	if (cy < screen_height) begin
+		if (USE_CRC && (cx == 0))
+			crc_enable <= 1'b1;
 
-	end else if ((cx < screen_width) && (cy < screen_height)) begin
-		if (fifo_read_en && !fifo_empty) begin
-			// regular output of FIFO data
-			hdmi_data <= {data_in[15:0], 8'h00};
-
-			// increment line payload counter
-			line_word_cnt <= line_word_cnt + 1'b1;
-		end else begin
-			// output idle counter
-			hdmi_data <= {idle_counter[15:8], idle_counter[7:0], 8'h00};
-
-			// increment idle counter
-			idle_counter <= idle_counter + 1'b1;
+		if (USE_CRC && (cx == screen_width+1)) begin
+			last_line_crc <= crc_out;
+			crc_enable <= 1'b0;
 		end
-	end else
-		line_word_cnt <= 16'h0000;
 
-	// Enable reading before beginning of next line
-	if ((cx == frame_width-1) && (cy < screen_height-1) && !fifo_empty)
-		fifo_read_en = 1'b1;
+		if (cx == screen_width-1) begin
+			// last word of line contains counter of words per line
+			hdmi_data <= {status_nibble[3:0], line_word_cnt[11:0], 8'h00};
+
+		end else if (USE_CRC && (cx == screen_width-2)) begin
+			// second last word contains CRC
+			hdmi_data <= {last_line_crc, 8'h00};
+
+		end else if (cx < screen_width) begin
+			if (fifo_read_en && !fifo_empty) begin
+				// regular output of FIFO data
+				hdmi_data <= {data_in[15:0], 8'h00};
+
+				// increment line payload counter
+				line_word_cnt <= line_word_cnt + 1'b1;
+			end else begin
+				// output idle counter
+				hdmi_data <= {idle_counter[15:8], idle_counter[7:0], 8'h00};
+
+				// increment idle counter
+				idle_counter <= idle_counter + 1'b1;
+			end
+		end else
+			line_word_cnt <= 16'h0000;
+
+		// Enable reading before beginning of next line
+		if ((cx == frame_width-1) && (cy != screen_height-1)) begin
+			if (!fifo_empty)
+				fifo_read_en = 1'b1;
+		end
+
+		// switch read off at end of line before sending the word counter
+		// -2 because the last word is reserved (line_word_cnt and metadata)
+		if (cx == screen_width-2-USE_CRC)
+			fifo_read_en = 1'b0;
+	end
 
 	// switch read off during blanking
 	if (cy > screen_height)
-		fifo_read_en = 1'b0;
-
-	// switch read off at end of line before sending the word counter
-	// -2 because the last word is reserved (line_word_cnt and metadata)
-	if ((cx == screen_width-2) && (cy < screen_height))
 		fifo_read_en = 1'b0;
 
 	// switch read off when FIFO has only one word remaining
@@ -97,10 +128,10 @@ always @(posedge clk_pixel) begin
 			9  : status_nibble <= frame_cnt[7:4];
 			10 : status_nibble <= frame_cnt[11:8];
 			11 : status_nibble <= frame_cnt[15:12];
+			14 : status_nibble <= { 3'b000, USE_CRC };
 			default : status_nibble <= 4'h0;
 		endcase
 	end
-
 end
 
 	wire tmds_clock;
